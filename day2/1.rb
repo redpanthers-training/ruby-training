1493a2294e20facf8332666a2626e3b0013af7bd
sameesh-s <sameesh.fisat@gmail.com>
2016-08-09 03:01:22 +0530

    Method missing Prblm

--- day2/1.rb
+++ day2/1.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 class Operator  
   def method_missing(m) 
-    number = m.to_s.split('_').last
+    number = m.to_s[m.size-10,m.size]
+    puts number
     if number.size==10  && number.to_i.to_s.size == number.size
       puts "Calling "+number
     else

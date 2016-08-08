7a9fe8276f97afdacb848d34ed9bb228aaa552d5
harisr92 <harikrishnansr92@live.com>
2016-08-08 18:00:39 +0530

    added files prime.rb,isprime.rb,reverse.rb,fizzBuzz.rb

---
+++ day1/isprime.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby -w
+
+f=1
+@i=0
+number=ARGV[0].to_i
+
+
+(2..number).map{|i| @i=i
+	break if number%i==0
+	f=1}
+
+if f==1 and number==@i
+	puts "#{number} is prime" 
+else
+	puts "#{number} is not a prime"
+end

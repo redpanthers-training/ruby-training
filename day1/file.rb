b57d68c86e0be8da4bc34e2e297077adea61f92d
iamfredy <fredy799@gmail.com>
2016-08-08 19:03:01 +0530

    Add files via upload

---
+++ day1/file.rb
@@ -0,0 +1,7 @@
+text=File.read("/home/iamfred/sample.txt")
+words=text.downcase.split.sort;
+out_file=File.new("/home/iamfred/out.csv","w")
+(0..words.size-1).each do |i|
+	entry=words[i]+","+words[i].length.to_s
+	out_file.puts(entry)
+end

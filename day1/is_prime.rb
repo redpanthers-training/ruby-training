e12133b3f7d26e6b68bccb01222ed91052a42cb4
iamfredy <fredy799@gmail.com>
2016-08-09 09:41:56 +0530

    Add files via upload

--- day1/is_prime.rb
+++ day1/is_prime.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 def print_it(x)
-	x=x.to_i
-	half=x/2;
-	status=true;
-	(2..half).each do |i|
-	rem=x%i;
-	status=false if rem==0;
-	break if status==false
-	end
-	status=true if x==2;
-	puts status
+  x=x.to_i
+  half=x/2;
+  status=true;
+  (2..half).each do |i|
+    rem=x%i;
+    status=false if rem==0;
+    break if status==false
+  end
+  status=true if x==2;
+  puts status
 end
 print_it(ARGV[0])

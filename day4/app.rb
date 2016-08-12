67a57b69cb9e6960402de3d74f5858ff55cacf45
mebinthomasmalpan <mebinthomasmalpan@gmail.com>
2016-08-12 15:31:43 +0530

    Json Format Page Scrapping

--- day4/app.rb
+++ day4/app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'net/http'
 require 'open-uri'
 require 'nokogiri'
+require 'json'
 
 get "/" do
   erb :index
@@ -9,14 +10,23 @@ end
 
 post "/result" do
   begin
+    json_hash = {}
     @domain_name = params[:domain_name]
     uri = URI(@domain_name)
     @doc = Nokogiri::HTML(open("https://#{uri}"))
-    @head  = @doc.css('h1,h2,h3,h4,h5,h6')
-    @links = @doc.css('a')
+    content_type :json
+    json_hash['h1']=@doc.css('h1').map {|h| h.text}
+    json_hash['h2']=@doc.css('h2').map {|h| h.text}
+    json_hash['h3']=@doc.css('h3').map {|h| h.text}
+    json_hash['h4']=@doc.css('h4').map {|h| h.text}
+    json_hash['h5']=@doc.css('h5').map {|h| h.text}
+    json_hash['h6']=@doc.css('h6').map {|h| h.text}
+    json_hash['links']=@doc.css('a').map {|a| a.text}
+    file = File.open("out.txt", "w+") {|f| f.write(JSON.pretty_generate(json_hash)) }
+    @data = JSON.pretty_generate(json_hash)
   rescue
    'Oops'
   end
-  erb :result  
+  #erb :result  
 end
 

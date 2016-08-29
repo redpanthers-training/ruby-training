91efb9398c876fa116d963f0e4d896cc866163a5
Arunkumar V S <vsarunkumar@yesdigital.in>
2016-08-29 14:14:01 +0530

    changed fixed number to constants

--- day1/prime.rb
+++ day1/prime.rb
@@ -1,16 +1,21 @@
-#!/usr/bin/env ruby
+	#!/usr/bin/env ruby
 
-max = 1000000000
+	class Prime
 
-def prime_numbers max
-	print "Prime numbers between 0 and #{max} are...\n"
-	
-    for i in (2..max) do
-        for j in (2..i) do
-            break if i%j == 0
-        end
-        print "#{i} \n" if i == j
-    end
-end
+		MAX = 100
 
-prime_numbers max
+		def prime_numbers 
+			print "Prime numbers between 0 and #{MAX} are...\n"
+			
+		    for i in (2..MAX) do
+		        for j in (2..i) do
+		            break if i%j == 0
+		        end
+		        print "#{i} \n" if i == j
+		    end
+		end		# end method prime_number
+
+	end		#end class Prime	
+
+	object = Prime.new()
+	object.prime_numbers

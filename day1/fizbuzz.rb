b02d10753b6080df3b52e92919d736c642e2c0ba
Arunkumar V S <vsarunkumar@yesdigital.in>
2016-08-29 16:19:53 +0530

    Fizbuzz challenge

---
+++ day1/fizbuzz.rb
@@ -0,0 +1,24 @@
+	#! usr/bin/env ruby
+
+	class Fizbuzz
+
+		NUM = 15
+
+		def print_num
+			for i in (0..NUM)  do 
+				if(i%5 == 0 && i%3 == 0)
+					puts "FizzBuzz"
+				elsif i%3 == 0
+				 	puts "Fizz"
+				elsif i%5 == 0
+				 	puts "Buzz"
+				else
+					puts i
+				end    # if else statement
+			end    #end for loop
+		end    # end method print_num
+
+	end		# end class Fizbuzz
+
+	object = Fizbuzz.new()
+	object.print_num

9d1f364c4fddaed1d740e012d4ca32b3a75aee3e
anjanakrshnangit config --global user.name anjanakrshnan <anjanakrish.p@gmail.com>
2016-10-28 10:00:35 +0530

    file modified

--- day2/answers/day2.rb
+++ day2/answers/day2.rb
@@ -3,12 +3,11 @@ class Operator
  	my_string=method_name
  	int="#{my_string}".split(//).last(10).join
 	puts int
- 	if int.size == 10 && int.to_i.to_s.size
+ 	if int.size == 10 && int.to_i.to_s.size == int.size
  		print "calling.."
  	else
- 		print "not an integer"
+ 		print "not a valid number"
  	end
  end	
 end
 Operator.new.call_1234567890
-
